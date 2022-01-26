c2459ecce21466fbc95a8d689563aece51e894c1
Long Kelvin <longkelvin101099@gmail.com>
2022-01-26 20:30:50 +0700

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SimpleGlassTheme_AspNetCore
  A simple glass theme using HTML&CSS
+ Apply for web, it's just a small project to practices HTML and CSS.
---
+++ WebUI Project/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI_Project.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
